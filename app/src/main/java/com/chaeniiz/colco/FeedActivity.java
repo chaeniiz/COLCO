9a184f249d9993f25bfb7838a0821880da5d3c0b
송채윤 <indexor18@gmail.com>
2018-05-02 18:30:23 +0900

    add splash

--- app/src/main/java/com/chaeniiz/colco/FeedActivity.java
+++ app/src/main/java/com/chaeniiz/colco/FeedActivity.java
@@ -7,6 +7,8 @@ public class FeedActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
+        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
     }
