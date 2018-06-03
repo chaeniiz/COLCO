582fb69ec0e0b4ce5090f4f2233bbbe16b26d317
송채윤 <indexor18@gmail.com>
2018-06-04 01:34:35 +0900

    add DatePicker at SettingActivity

--- app/src/main/java/com/chaeniiz/colco/ui/activity/SettingActivity.java
+++ app/src/main/java/com/chaeniiz/colco/ui/activity/SettingActivity.java
@@ -1,14 +1,41 @@
 package com.chaeniiz.colco.ui.activity;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.chaeniiz.colco.R;
 
+import java.util.Calendar;
+
 public class SettingActivity extends BaseActivity {
+
+    public static TextView tvBirth;
+
+    public static class DatePickerFragment extends android.support.v4.app.DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            tvBirth.setText(year + "-" + (month + 1) + "-" + day);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +60,19 @@ public class SettingActivity extends BaseActivity {
                 finish();
             }
         });
+
+        tvBirth = (TextView) findViewById(R.id.tv_birth);
+        tvBirth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDatePickerDialog(view);
+            }
+        });
+    }
+
+    public void showDatePickerDialog(View v) {
+        android.support.v4.app.DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 
     @Override
