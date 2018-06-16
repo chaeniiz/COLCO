f95cf1be92b1f6ca789c615ca04a108c4bc140a6
송채윤 <indexor18@gmail.com>
2018-06-16 21:46:14 +0900

    add scrap toolbar

--- app/src/main/java/com/chaeniiz/colco/ui/activity/ScrapActivity.java
+++ app/src/main/java/com/chaeniiz/colco/ui/activity/ScrapActivity.java
@@ -3,6 +3,7 @@ package com.chaeniiz.colco.ui.activity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.support.v7.widget.Toolbar;
 
 import com.chaeniiz.colco.R;
 import com.chaeniiz.colco.data.model.Scrap;
@@ -21,19 +22,26 @@ public class ScrapActivity extends BaseActivity {
         setTheme(R.style.Theme_AppCompat_Light_NoActionBar);
         super.onCreate(savedInstanceState);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         rvScrap = findViewById(R.id.rv_scrap);
 
         StaggeredGridLayoutManager sgm = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
         rvScrap.setLayoutManager(sgm);
 
         List<Scrap> items = new ArrayList<>();
-        Scrap[] item = new Scrap[4];
+        Scrap[] item = new Scrap[8];
         item[0] = new Scrap(R.drawable.thumbnail_product, "MAC", "루비우", "여름 쿨", "65%");
         item[1] = new Scrap(R.drawable.thumbnail_product, "MAC", "루비우", "여름 쿨", "65%");
         item[2] = new Scrap(R.drawable.thumbnail_product, "MAC", "루비우", "여름 쿨", "65%");
         item[3] = new Scrap(R.drawable.thumbnail_product, "MAC", "루비우", "여름 쿨", "65%");
+        item[4] = new Scrap(R.drawable.thumbnail_product, "MAC", "루비우", "여름 쿨", "65%");
+        item[5] = new Scrap(R.drawable.thumbnail_product, "MAC", "루비우", "여름 쿨", "65%");
+        item[6] = new Scrap(R.drawable.thumbnail_product, "MAC", "루비우", "여름 쿨", "65%");
+        item[7] = new Scrap(R.drawable.thumbnail_product, "MAC", "루비우", "여름 쿨", "65%");
 
-        for(int i = 0; i < 4; i++) {
+        for(int i = 0; i < 8; i++) {
             items.add(item[i]);
         }
 
